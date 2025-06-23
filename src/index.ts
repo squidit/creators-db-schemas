b1bbf76952ed5fa447d8871f4bc644c458b7d5e6
Joao Monteiro <joao.monteiro@wake.tech>
2025-06-23 11:57:38 -0300

    Adicionando schema/model de recruitments do mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 
-export * as businessIntelligenceDb from './databases/business_intelligence/schema';
-export * as influencersDb from './databases/influencers/schema';
-export * as npsDb from './databases/nps/schema';
-export * as paymentDb from './databases/payment/schema';
+export * as recruiment from './databases/mongodb/influencers/recruitment';
+export * as businessIntelligenceDb from './databases/mysql/business_intelligence/schema';
+export * as influencersDb from './databases/mysql/influencers/schema';
+export * as npsDb from './databases/mysql/nps/schema';
+export * as paymentDb from './databases/mysql/payment/schema';
 
